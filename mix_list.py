154db0cb1dcf3bc1d41a7a2a0b75a8e19db967ac
brahmanand.moholkar@gmail.com <brahmanand.moholkar@gmail.com>
2019-06-24 11:20:46 -0400

    Lists

---
+++ mix_list.py
@@ -0,0 +1,11 @@
+
+# !/usr/bin/python3
+
+my_list= ["192.168.0.5" , 5080, "UP"]
+
+print("The first item in the list (IP): " + my_list[0])
+
+print("The second item in the list (state): " + str( my_list[1]))
+
+print("third :" + my_list[2])
+
---
+++ new_list.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+
+new_list = [5060 , "80" , 55 , "10.0.0.1" , "10.20.30.1" , "ssh" ]
+
+
+print(f"When I SSH into IP address {new_list[3]} or {new_list[4]} I am unable to ping ports {new_list[0]}, {new_list[1]}, or {new_list[2]}.")
+
+
+
